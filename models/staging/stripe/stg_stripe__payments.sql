cfddcc91e4313d419ceb60f654b4eea44e639e83
saurabhsrivastava.etl@gmail.com <saurabhsrivastava.etl@gmail.com>
2025-05-18 09:31:34 +0000

    adding source concept in stripe and jaffle_shop yaml and changes in sql file

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,4 +3,4 @@ select id as payment_id
  -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
- from raw.stripe.payment
+ from {{ source('stripe','payment') }}
